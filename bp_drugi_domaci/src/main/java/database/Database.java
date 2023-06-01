2bed45612f98146e4f55b5906424e845fe7d8a38
Sofija Savic <sofsav>
2023-06-01 02:33:51 +0200

    konektovanje - MongoDB

--- bp_drugi_domaci/src/main/java/database/Database.java
+++ bp_drugi_domaci/src/main/java/database/Database.java
@@ -6,7 +6,5 @@ import data.Row;
 import java.util.List;
 
 public interface Database {
-
-
     List<Row> getDataFromTable(String from);
 }
--- bp_drugi_domaci/src/main/java/utils/Constants.java
+++ bp_drugi_domaci/src/main/java/utils/Constants.java
@@ -2,9 +2,9 @@ package utils;
 
 public class Constants {
 
-    public static final String MYSQL_IP = "134.209.239.154";
-    public static final String MYSQL_DATABASE = "bp_tim35";
-    public static final String MYSQL_USERNAME = "writer";
-    public static final String MYSQL_PASSWORD = "qwoL6NicEhmL7emn";
+    public static final String MONGO_IP = "134.209.239.154";
+    public static final String MONGO_DATABASE = "bp_tim35";
+    public static final String MONGO_USERNAME = "writer";
+    public static final String MONGO_PASSWORD = "qwoL6NicEhmL7emn";
 
 }
